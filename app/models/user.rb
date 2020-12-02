154868de537bf05c9d52408d625a69e566f21b90
nakamurayuto12 <73273993+nakamurayuto12@users.noreply.github.com>
2020-12-02 21:39:20 +0900

    veiwファイルの設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :birthday, presence: true
+  validates :surname, presence: true
+  validates :name, presence: true
+  validates :surname_furigana, presence: true
+  validates :name_furigana, presence: true
+  validates :nickname, presence: true
 end
