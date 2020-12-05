9c0f039ef8b670ba97ec007e62a080a329826b58
nakamurayuto12 <73273993+nakamurayuto12@users.noreply.github.com>
2020-12-05 18:49:26 +0900

    テストコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    surname               {person.last.kanji}
+    name                  {person.first.kanji}
+    surname_furigana      {person.last.katakana}
+    name_furigana         {person.first.katakana}
+    birthday              {Faker::Date.backward}
+  end
+end
