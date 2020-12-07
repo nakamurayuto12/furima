4d19a3709b4471fa36bc87ddecb03a00646777af
nakamurayuto12 <73273993+nakamurayuto12@users.noreply.github.com>
2020-12-07 18:14:50 +0900

    ActiveHashのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :shipment_source
+  belongs_to :day_to_ship
+
+  validates :category_id,          numericality: { other_than: 1 }
+  validates :status_id,            numericality: { other_than: 1 }
+  validates :shipping_charge_id,   numericality: { other_than: 1 }
+  validates :shipment_source_id,   numericality: { other_than: 0 }
+  validates :day_to_ship_id,       numericality: { other_than: 1 }
 end
