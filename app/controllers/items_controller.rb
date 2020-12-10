21a8ddce78d3b79df4162c2a75b8066e68466640
nakamurayuto12 <73273993+nakamurayuto12@users.noreply.github.com>
2020-12-10 12:53:09 +0900

    Revert "エラー文の表示"
    This reverts commit 83a8edc292d682c12786d62a02f6ca9307864cd4.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,14 +27,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
+    item = Item.find(params[:id])
+    item.update(item_params)
   private
 
   def item_params
